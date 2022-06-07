816c6bd7024f0f2a36bae51721e1d14a0d378078
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2022-06-07 07:23:26 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 // import './App.css';
-import Products from './component/Products/Products'
+// import Products from './component/Products/Products';
+// import Navbar from './component/Navbar/Navbar'
+
+import { Products, Navbar } from './components';
 
 function App() {
   return (
     <div className="App">
-     
+     <Navbar />
      <Products />
     </div>
   );
