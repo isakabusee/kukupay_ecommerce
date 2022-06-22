b6e6d54d66c78ed45c93cd83f0002e472c493e2c
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2022-06-22 01:26:25 -0700

    some progress

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 // import './App.css';
 // import Products from './component/Products/Products';
 // import Navbar from './component/Navbar/Navbar'
+import React, { useState, useEffect } from 'react';
+import { commerce } from './lib/commerce';
 
 import { Products, Navbar } from './components';
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  }
+  useEffect(() => {
+    fetchProducts();
+  },[]);
+
+  console.log(products);
   return (
     <div className="App">
      <Navbar />
