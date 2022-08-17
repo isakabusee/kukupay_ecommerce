1be1cdb6f5f14db7a716813d62fb6d17ee6d87f7
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2022-08-17 01:18:16 -0700

    cart complete

--- src/App.js
+++ src/App.js
@@ -33,8 +33,8 @@ function App() {
   return (
     <div className="App">
      <Navbar totalItems={cart.total_items} />
-     <Products products={products} onAddToCart={handleAddToCart} />
-     {/* <Cart cart={cart} /> */}
+     {/* <Products products={products} onAddToCart={handleAddToCart} /> */}
+     <Cart cart={cart} />
     </div>
   );
 }
